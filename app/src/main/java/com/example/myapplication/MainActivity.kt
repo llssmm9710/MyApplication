3a41a2af5100135fb6331806c613d4f4e7a2978e
S-M Lee <leessung97@daum.net>
2020-03-31 22:20:15 +0900

    lifecycleLog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private static final String TAG = "MyActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e(TAG,"onCreate() called!");
     }
 
     override fun onResume() {
         super.onResume()
+        Log.e(TAG,"onResume() called!");
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"Sungmin's onSrart() called!");
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG,"Sungmin's onResrart() called! Baam!");
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG,"Sungmin's onStop() called! Baam!");
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG,"Sungmin's onPause() called! Baam!");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG,"Sungmin's onDestroy() called! Baam!");
     }
 }
